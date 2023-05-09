26d212e0443d23217a3d3b08673b33f7752c479f
Grzegorz Smoktunowicz <biarth@wp.pl>
2023-05-09 21:16:56 +0200

    graphql section

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -6,16 +6,30 @@ const Movie = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    let myMovie = {
-      id: 1,
-      title: "Highlander",
-      release_date: "1986-03-07",
-      runtime: 116,
-      mpaa_rating: "R",
-      description: "Some long description",
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
     };
-    setMovie(myMovie);
+
+    fetch(`/movies/${id}`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovie(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [id]);
+
+  if (movie.genres) {
+    movie.genres = Object.values(movie.genres);
+  } else {
+    movie.genres = [];
+  }
+
   return (
     <div>
       <h2>Movie: {movie.title}</h2>
@@ -25,7 +39,22 @@ const Movie = () => {
           {movie.mpaa_rating}
         </em>
       </small>
+      <br />
+      {movie.genres.map((g) => (
+        <span key={g.genre} className="badge bg-secondary me-2">
+          {g.genre}
+        </span>
+      ))}
       <hr />
+
+      {movie.image !== "" && (
+        <div className="mb-3">
+          <img
+            src={`https://image.tmdb.org/t/p/w200/${movie.image}`}
+            alt="poster"
+          />
+        </div>
+      )}
       <p>{movie.description}</p>
     </div>
   );
